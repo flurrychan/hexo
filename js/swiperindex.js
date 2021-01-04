0c1a48a44763faf1df662228e6718dc8f2679de7
flurrychan <63506003+flurrychan@users.noreply.github.com>
2021-01-04 21:49:08 +0800

    Add files via upload

---
+++ js/swiperindex.js
@@ -0,0 +1,27 @@
+let swiper = new Swiper('.blog-slider', {
+	passiveListeners:true,
+	spaceBetween: 30,
+	effect: 'fade',
+	loop: true,
+    autoplay: {
+    disableOnInteraction: true,
+	delay:3000
+    },
+	mousewheel:false,
+	// autoHeight: true,
+	pagination: {
+		el: '.blog-slider__pagination',
+		clickable: true,
+	}
+});
+window.οnlοad=function(){
+  let comtainer = document.getElementById('swiper_container');
+      comtainer.onmouseenter = function () {
+        swiper.autoplay.stop();
+      };
+      comtainer.onmouseleave = function () {
+        swiper.autoplay.start();
+	  }
+	}
+
+
