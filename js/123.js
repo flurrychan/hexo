fd8125923d05d31bccbe785779b0268bdec4bcfe
flurrychan <63506003+flurrychan@users.noreply.github.com>
2021-01-05 01:41:04 +0800

    Add files via upload

---
+++ js/123.js
@@ -0,0 +1,8 @@
+$(".category-list-count").prepend("<i class=\"fas fa-book\"></i>");
+$(".category-list-item").eq(0).insertAfter($(".gallery-group-name").eq(0))
+$(".category-list-item").eq(1).insertAfter($(".gallery-group-name").eq(1))
+$(".category-list-item").eq(2).insertAfter($(".gallery-group-name").eq(2))
+$(".category-list-item").eq(3).insertAfter($(".gallery-group-name").eq(3))
+$(".category-list-item").eq(4).insertAfter($(".gallery-group-name").eq(4))
+$(".category-list-item").eq(5).insertAfter($(".gallery-group-name").eq(5))
+$(".category-list-item a").hide();
