879bc6fab0068f725f0cff65c82b74a15a02c5e3
flurrychan <63506003+flurrychan@users.noreply.github.com>
2021-01-08 15:12:02 +0800

    Update 123.js

--- js/123.js
+++ js/123.js
@@ -1,5 +1,3 @@
-ul
-{
 $(".category-list-count").prepend("<i class=\"fas fa-book\"></i>");
 $(".category-list-item").eq(0).insertAfter($(".gallery-group-name").eq(0))
 $(".category-list-item").eq(1).insertAfter($(".gallery-group-name").eq(1))
@@ -8,4 +6,4 @@ $(".category-list-item").eq(3).insertAfter($(".gallery-group-name").eq(3))
 $(".category-list-item").eq(4).insertAfter($(".gallery-group-name").eq(4))
 $(".category-list-item").eq(5).insertAfter($(".gallery-group-name").eq(5))
 $(".category-list-item a").hide();
-}
+
