0036197414a0b8413e346b23d9f3e53cd521eda1
flurrychan <63506003+flurrychan@users.noreply.github.com>
2021-01-22 17:00:46 +0800

    Update mirror.js

--- js/mirror.js
+++ js/mirror.js
@@ -7,11 +7,11 @@ function Mirror() {
     // if (hostname === '域名，不带https://协议')
     if (hostname === 'aabbj.com') { //如果是主站
       // url = "域名，带https://协议" + pathname;
-      url = "https://flurrychan.github.io" + pathname; //就跳转到镜像站的同名页面
+      url = "https://killua.cf" + pathname; //就跳转到镜像站的同名页面
       window.alert("即将为您跳转至镜像站");
       window.location.href = url;
     }
-    else if(hostname === 'flurrychan.github.io') {
+    else if(hostname === 'killua.cf') {
       url = "https://aabbj.com" + pathname;
       window.alert("当前为镜像站，即将返回主站");
       window.location.href = url;
